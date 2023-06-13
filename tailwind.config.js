0e04af93a83a2bbccbcf3901c7f020847067cf99
anotherITdude <abhi.baidya@gmail.com>
2023-06-13 16:49:11 +0200

    new: added/setup fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,14 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        'gotham-black': ['gotham-black'],
+        'gotham-black-italic': ['gotham-black-italic'],
+        'neosans-bold': ['neosans-bold'],
+        'neosans-medium': ['neosans-medium'],
+        'neosans-regular': ['neosans-regular'],
+        'primetime': ['primetime']
+      },
     },
   },
   plugins: [],
