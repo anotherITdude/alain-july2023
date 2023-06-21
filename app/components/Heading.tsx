da49dfcaaa5c9e2ebfa2f846deebb538b4d34a9b
anotherITdude <abhi.baidya@gmail.com>
2023-06-21 15:32:18 +0200

    fix: asterisk fix

--- app/components/Heading.tsx
+++ app/components/Heading.tsx
@@ -10,14 +10,14 @@ const Heading: React.FC<HeadingProps> = ({ title, asterisk, className }) => {
   return (
     <div>
       {title && (
-        <div className="`${className}` heading-2 shadow_h2 flex text-xl  text-white pt-4 pl-2">
+        <div className="`${className}` heading-2  flex text-xl  text-[#002E6D] pt-4 pl-">
           {asterisk && (
             <Image
               quality={100}
               priority={true}
-              width={15}
+              width={10}
               alt="important"
-              className="mt-[-8px]"
+              className="mt-[-8px] mr-1"
               src={asteriskImg}
             />
           )}
