3005304c03f67255454e587e5bf166fc4608b931
anotherITdude <abhi.baidya@gmail.com>
2023-07-31 15:24:11 +0200

    fix: ar favicon fix

--- app/ar/page.tsx
+++ app/ar/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <SchoolYear />
       <Scholarships />
       <HowTo />
-      <Registration id="register" />
+      <Registration />
       <Terms />
     </Container>
   );
